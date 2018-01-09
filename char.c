c993bbafa2d73955d16696c23d5ecdb4a96577ce
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-01-09 16:54:26 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char x1;
+printf("enter the value");
+scanf("%s",&x1);
+if(x1=='a'||x1=='e'||x1=='i'||x1=='o'||x1=='u'||x1=='b'||x1=='c'||x1=='d'||x1=='f'||x1=='g'||x1=='h'||x1=='j'||x1=='j'||x1=='k'||x1=='l'||x1=='m'||x1=='n'||x1=='p'||x1=='q'||x1=='r'||x1=='s'||x1=='t'||x1=='v'||x1=='w'||x1=='x'||x1=='y'||x1=='z')
+{
+	printf("character");
+}
+else
+{
+	printf("not a character");
+}
+getch();
+}
