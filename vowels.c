2c77e04544292c86220e6cbfcfbdf40188d6e529
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-01-09 16:42:35 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the value");
+scanf("%s",&n);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+{
+	printf("vowels");
+}
+else
+{
+	printf("constants");
+}
+getch();
+}
