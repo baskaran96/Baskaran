93c9e4b39a10a344e7a06186a2820096fac56afd
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-01-11 16:36:32 +0530

    Create hlo.c

---
+++ hlo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    printf("enter the number");
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        printf("hello");
+    }
+    getch();
+}
