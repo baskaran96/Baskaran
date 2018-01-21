fcb025054acd50f45b08cd77e3de0ccb68d5d5fb
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-01-21 11:36:12 +0530

    Create mn.c

---
+++ mn.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,x,y,z;
+printf("enter the number of elements");
+scanf("%d%d%d",&x,&y,&z);
+if(x<y)
+{
+  if(x<z)
+{
+printf("%d",x);
+}
+}
+else if(y<z)
+{
+printf("%d",y);
+}
+else
+{
+printf("%d",z);
+
+}
+}
