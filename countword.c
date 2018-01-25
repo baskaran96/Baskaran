311a5c75a4662b2cd52c309b9eaf65ce25abfcd1
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-01-25 16:06:11 +0530

    Create countword.c

---
+++ countword.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int x,i,j;
+printf("enter the number of words");
+scanf("%d",&x);
+for(i=0;i<5;i++)
+{
+for(j=0;j<5;j++)
+{
+printf("count the number of characters");
+}
+}
+}
