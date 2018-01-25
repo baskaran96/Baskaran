299d0dd5875385d84e60bb2c9ebdb35c72746ca9
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-01-25 15:59:56 +0530

    Create countchar.c

---
+++ countchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a[i];
+string str="good mobile";
+printf("Enter the character: ");
+scanf("%d",&a[i]);
+str=strlen(good mobile);
+scanf("%s",&str);
+return 0;
+}
