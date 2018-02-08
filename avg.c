83d9bd3edc2bfed9685926c0c00a9fa706e349d6
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-02-08 08:11:40 -0300

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,17 @@
+#include"stdio.h"
+int main()
+{
+int a,x,s=0,n1,i;
+float avg;
+printf("\nEnter the no. of elements");
+scanf("\n%d",&a);
+printf("\nEnter the elements");
+for(i=0;i<x;++i)
+{
+scanf("\n%d",&n1);
+s=s+n1;
+}
+avg=s/a;
+printf("\n Average=%f",x,avg);
+return 0;
+}
