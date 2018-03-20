4471b82bcee472b384d4a8036d9aae6391fea118
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-03-20 16:23:34 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float len,b,area;
+    scanf("%f",&len);
+    scanf("%f",&b);
+    area=len*b;
+    printf("the length and breath is : %0.5f",area);
+    return 0;
+}
