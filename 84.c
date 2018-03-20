5bf70a8ca91a3add0b910b7bd971609cde01f66f
baskaran96 <32239213+baskaran96@users.noreply.github.com>
2018-03-20 16:07:47 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char as;
+    printf("Enter the value\n");
+    scanf("%c",&as);
+    printf("the ascii value of given character is %c : %d",as,as);
+    return 0;
+}
